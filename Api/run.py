522b55c38646177514c91f67bfe2cf92fec3efb8
Ludi <rcellamen@yahoo.fr>
2026-02-25 14:54:42 +0100

    Ajout de la carte à la DB par la route (ajouter_une_carte)

--- Api/run.py
+++ Api/run.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from capteur import Lire_Badge
+from model import Badge
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///LB.db'
@@ -10,11 +12,15 @@ db = SQLAlchemy(app)
 @app.route("/ajouter_une_carte", methods=["GET", "POST"])
 def ajouter_une_carte():
     id, text = Lire_Badge(False)
-    return {id, text}, 201
+    carte = Badge(id_badge=id, texte=text, date_ajout=datetime.now())
+    db.session.add(carte)
+    db.session.commit()
+    # Retourne un dictionnaire propre
+    return {"id": id, "texte": text}, 201
 
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=True)
 
