64c76f7d4880e08940dd480bda20545dfa844a4a
Ludi <rcellamen@yahoo.fr>
2026-02-25 13:47:57 +0100

    Modification de la route ajouter_une_carte (lecture de badge RFID)

--- Api/run.py
+++ Api/run.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from capteur import Lire_Badge
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///LB.db'
@@ -9,8 +9,8 @@ db = SQLAlchemy(app)
 
 @app.route("/ajouter_une_carte", methods=["GET", "POST"])
 def ajouter_une_carte():
-    print("tzqd")
-    return {"message": "Carte ajoutée"}, 201
+    id, text = Lire_Badge(False)
+    return {id, text}, 201
 
 
 if __name__ == "__main__":
