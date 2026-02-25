76e33b89d2e35b4cc41602761a57994c345fa9e1
Ludi <rcellamen@yahoo.fr>
2026-02-25 16:23:06 +0100

    Ajout route pour modifier la carte (/modifier_une_carte)

--- Api/run.py
+++ Api/run.py
@@ -1,23 +1,28 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from capteur import Lire_Badge
+from db import db
 from model import Badge
 from datetime import datetime
+from config import app
+from flask import request
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///LB.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
-@app.route("/ajouter_une_carte", methods=["GET", "POST"])
+@app.route("/ajouter_une_carte", methods=["GET"])
 def ajouter_une_carte():
     id, text = Lire_Badge(False)
-    carte = Badge(id_badge=id, texte=text, date_ajout=datetime.now())
+    carte = Badge(id_badge=id, val_badge=text, date_ajout=datetime.now())
     db.session.add(carte)
     db.session.commit()
     # Retourne un dictionnaire propre
     return {"id": id, "texte": text}, 201
 
+@app.route("/modifier_une_carte/<id_badge>", methods=["GET", "POST"])
+def modifier_une_carte(id_badge):
+    if  request.methods == "GET":
+        carte = Badge.query.filter_by(id_badge=id_badge).first()
+    
+        if carte:    
+            return {"id": carte.id_badge, "texte": carte.val_badge, "id_util": carte.id_utilisateur, "derniere_connexion": carte.der_connexion, "date_ajout": carte.date_ajout}, 201
+    return {"error": "Carte not found"}, 404
 
 if __name__ == "__main__":
     with app.app_context():
